e47efaa735f812226a404704e98f77d5381fb6ec
Michael <wanjikumichaelgacha@gmail.com>
2017-10-13 22:11:49 +0300

    second

---
+++ divide_two_numbers.py
@@ -0,0 +1,12 @@
+
+
+def divide_two_numbers (x,y):
+
+
+    x = raw_input('Please enter a number')
+    y = raw_input('Please enter a number')
+   
+if x >=2
+    return x/y
+else:
+    print ('Please change the value of X')
--- index.py
+++ index.py
@@ -1,1 +1,4 @@
-print( "Hello World")
+x = 10
+y = x+1
+
+print(y)
